b2f6a0968850f75678285fde95b4d9b4a7ce28cc
Kenneth van Ewijk <kennyboy@live.nl>
2015-09-04 14:00:16 +0200

    Completed first version

--- ErgometerUitlezer/ComPort.cs
+++ ErgometerUitlezer/ComPort.cs
@@ -9,11 +9,57 @@ namespace ErgometerUitlezer
 {
     class ComPort
     {
-        SerialPort comPort;
+        private SerialPort comPort;
 
         public ComPort()
         {
             comPort = null;
+        }
+
+        public Boolean Connect(string port)
+        {
+            comPort = new SerialPort();
+            comPort.PortName = port;
+            comPort.DataBits = 8;
+            comPort.Parity = Parity.None;
+            comPort.StopBits = StopBits.One;
+            comPort.BaudRate = 9600;
+
+            comPort.Open();
+
+            return comPort.IsOpen;
+        }
+
+        public Boolean Disconnect()
+        {
+            comPort.Close();
+            return !comPort.IsOpen;
+        }
+
+        public Boolean IsOpen()
+        {
+            if (comPort != null)
+                return comPort.IsOpen;
+            else
+                return false;
+        }
+
+        public void Write(string input)
+        {
+            if(IsOpen())
+            {
+                comPort.WriteLine(input);
+            }
+        }
+
+        public string Read()
+        {
+            if (IsOpen())
+            {
+                return comPort.ReadLine();
+            }
+
+            return "";
         }
     }
 }
