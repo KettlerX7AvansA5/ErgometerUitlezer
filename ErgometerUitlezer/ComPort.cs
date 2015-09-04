b3d31195b611336cb3a9986053fd4021b1f21d26
Kenneth van Ewijk <kennyboy@live.nl>
2015-09-04 12:15:59 +0200

    Added basic read functionality

---
+++ ErgometerUitlezer/ComPort.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErgometerUitlezer
+{
+    class ComPort
+    {
+        SerialPort comPort;
+
+        public ComPort()
+        {
+            comPort = null;
+        }
+    }
+}
---
+++ ErgometerUitlezer/Meting.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErgometerUitlezer
+{
+    class Meting
+    {
+        public int HeartBeat { get; }
+        public int RPM { get; }
+        public double Speed { get; }
+        public double Distance { get; }
+        public int Power { get; }
+        public int Energy { get; }
+        public int Seconds { get; }
+        public int ActualPower { get; }
+
+        public Meting(int heartbeat, int rpm, double speed, double distance, int power, int energy, int seconds, int actualpower)
+        {
+            HeartBeat = heartbeat;
+            RPM = rpm;
+            Speed = speed;
+            Distance = distance;
+            Power = power;
+            Energy = energy;
+            Seconds = seconds;
+            ActualPower = actualpower;
+        }
+
+        public override string ToString()
+        {
+            string temp = "";
+            temp += "Heartbeat: " + HeartBeat + "\n";
+            temp += "RPM: " + RPM + "\n";
+            temp += "Speed: " + Speed + "\n";
+            temp += "Distance: " + Distance + "\n";
+            temp += "Power: " + Power + "\n";
+            temp += "Energy: " + Energy + "\n";
+            temp += "Seconds: " + Seconds + "\n";
+            temp += "ActualPower: " + ActualPower + "\n";
+            return temp;
+        }
+    }
+}
